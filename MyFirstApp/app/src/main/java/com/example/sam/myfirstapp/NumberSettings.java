fed15b59b7b107141133d1aaca4f46ddedefb8df
SamNettleton <nettleton.sam@gmail.com>
2017-02-20 13:55:15 -0600

    Added spinner for amount random, no functionality yet

--- MyFirstApp/app/src/main/java/com/example/sam/myfirstapp/NumberSettings.java
+++ MyFirstApp/app/src/main/java/com/example/sam/myfirstapp/NumberSettings.java
@@ -5,9 +5,14 @@ import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.support.design.widget.Snackbar;
 import android.widget.Button;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class NumberSettings extends AppCompatActivity {
 
@@ -16,6 +21,8 @@ public class NumberSettings extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.number_settings);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id
+                .coordinatorLayout);
 
         //set default numbers in min/max value spaces
         TextView minValueField = (TextView) findViewById(R.id.min_field);
@@ -23,8 +30,32 @@ public class NumberSettings extends AppCompatActivity {
         minValueField.setText(String.valueOf(MainActivity.randMin));
         maxValueField.setText(String.valueOf(MainActivity.randMax));
 
-        coordinatorLayout = (CoordinatorLayout) findViewById(R.id
-                .coordinatorLayout);
+        //set up for amount random spinner
+        Spinner amountRandom = (Spinner) findViewById(R.id.amount_random);
+        ArrayList<String> spinnerVals = new ArrayList<String>();
+        for (int i=0; i<11; i++){
+            spinnerVals.add(Integer.toString(i));
+        }
+        // Creating adapter for spinner so it shows array
+        ArrayAdapter<String> adaptAmountRandom = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_item, spinnerVals);
+        // Drop down layout style - list view with radio button
+        adaptAmountRandom.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // attaching data adapter to spinner
+        amountRandom.setAdapter(adaptAmountRandom);
+
+        // creating listener for the spinner
+        amountRandom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         //Add a listener for confirm settings button
         Intent intent = getIntent();
         Button btnInvalidRange = (Button) findViewById(R.id.confirm_settings);
